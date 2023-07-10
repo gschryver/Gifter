6c731b61c63e67a68d73aa32ccc6857c09b8936e
Gabrielle Schryver <gabbyschryver@gmail.com>
2023-07-10 10:57:20 -0400

    init

---
+++ Gifter/client/src/components/ApplicationViews.js
@@ -0,0 +1,24 @@
+import { Routes, Route, Navigate} from "react-router-dom";
+import PostList from "./PostList";
+import PostForm from "./PostForm";
+import PostDetails from "./PostDetails";
+
+const ApplicationViews = () => {
+
+return (
+     <Routes>
+     
+        <Route path="/" element= {<PostList />} />
+        
+        <Route path="/posts/add" element={<PostForm />} />
+                        
+        <Route path="*" element={<p>Whoops, nothing here...</p>} />
+     
+     </Routes>
+    
+    )
+  
+
+};
+
+export default ApplicationViews;
